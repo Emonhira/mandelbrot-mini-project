44caec94777737e3457dd189192898ff44a28e60
Emonhira <abdullahalmehedihira@gmail.com>
2026-03-11 10:44:56 +0100

    visualization part added

--- analysis.py
+++ analysis.py
@@ -26,5 +26,19 @@ for res in resolutions:
     
     benchmarks.append([res, t_naive, t_numpy, t_numba])
 
+import matplotlib.pyplot as plt
 
+def visualize_mandelbrot(data):
+    plt.figure(figsize=(10, 7))
+    # 'magma' or 'inferno' are great for showing detail
+    img = plt.imshow(data, extent=[X_MIN, X_MAX, Y_MIN, Y_MAX], cmap='magma')
+    plt.colorbar(img, label='Iteration depth (normalized)')
+    plt.title("Mandelbrot Set Visualization")
+    plt.xlabel("Re(c)")
+    plt.ylabel("Im(c)")
+    plt.show()
+
+# Example usage with the Numba version
+result = mandelbrot_numba(1024, 1024)
+visualize_mandelbrot(result)
 
